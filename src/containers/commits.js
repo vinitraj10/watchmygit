fc15e00fd8ebfc0ab4fe2d820534d5a063c3a41b
vinitraj10 <ent.vinit01@gmail.com>
2017-08-09 11:29:06 +0530

    commit container added

--- src/containers/commits.js
+++ src/containers/commits.js
@@ -1,8 +1,11 @@
 import React,{Component} from "react";
 import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import {getCommits} from "../actions";
 
 class Commits extends Component{
 	render(){
+		console.log("commits detail ", this.props);
 		return(
 			<div className="column col-6">
 				<figure className="avatar badge avatar-xl" data-badge="8" data-initial="YZ">
@@ -19,5 +22,8 @@ function mapStateToProps(state){
 		repo:state.repo
 	}
 }
+function mapDistpatchToProps(dispatch){
+	return bindActionCreators({getCommits},dispatch);
+}
 
-export default connect(mapStateToProps)(Commits);
+export default connect(mapStateToProps,mapDistpatchToProps)(Commits);
