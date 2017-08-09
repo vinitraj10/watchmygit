958928ef4f02edfd972b0abc4e878daf3b3deffb
vinitraj10 <ent.vinit01@gmail.com>
2017-08-09 11:27:20 +0530

    Commit Reducers addedgit add src/reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import UserReducer from './reducer-user';
 import RepoReducer from "./reducer-repo";
+import CommitReducer from "./reducer-commit";
 
 const rootReducer = combineReducers({
 	git:UserReducer,
-	repo:RepoReducer
+	repo:RepoReducer,
+	commits:CommitReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer-commit.js
@@ -0,0 +1,13 @@
+const intialState = {
+	isFetching:false,
+	isFetched:true,
+	commits:[]
+}
+
+export default function(state=intialState,action){
+	switch(action.type){
+
+	}
+
+	return state;
+}
