c4ba25fe27431b4bc363d7630f59b6d1642f0555
vinitraj10 <ent.vinit01@gmail.com>
2017-08-09 11:09:14 +0530

    nodata and fetchig components added

---
+++ src/components/fetching.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Fetching = () =>{
+	return (
+		<div className="column col-10">
+			<div className="loading"></div>
+		</div>
+	)
+}
+export default Fetching;
---
+++ src/components/nodata.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Nodata = () =>{
+	return (
+		<div className="column col-10">
+			<h3>No data Fetched!!</h3>
+		</div>
+	)
+}
+export default Nodata;
