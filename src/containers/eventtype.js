890d172c6374d766e85a7b21eb4976dcabc1f783
vinitraj10 <ent.vinit01@gmail.com>
2017-08-22 03:00:00 +0530

    Event Component modified

--- src/containers/eventtype.js
+++ src/containers/eventtype.js
@@ -1,43 +1,9 @@
 import React,{Component} from "react";
-export default class EventType extends Component{
-	checktype(commit){
-		const push = "PushEvent";
-		const fork = "ForkEvent";
-		const create = "CreateEvent";
-		const star = "WatchEvent";
-		const del = "DeleteEvent";
-		const issue = "IssuesEvent";
-		const pull = "PullRequest"
+import moment from 'moment';
 
-		switch(commit.type){
-			case push:
-				return "label label-primary";
-				break;
-			case fork:
-				return "label label-warning";
-				break;
-			case create:
-				return "label label-success";
-				break;
-			case star:
-				return "label label-star";
-				break;
-			case del:
-				return "label label-del";
-				break;
-			case issue:
-				return "label label-issue";
-				break;
-			case pull:
-				return "label label-req";
-				break;
-			default:
-				return "label label-rest";
-				break;
-		}
-	}
+export default class EventType extends Component{
 	render(){
-		const span = this.checktype(this.props.commit);
-		return <span className={span}>{this.props.type}</span>
+		const time = moment(this.props.commit.created_at).format("MMM Do YY");    
+		return <span className="time"><i className="icon icon-time"></i> {time}</span>
 	}
 }
